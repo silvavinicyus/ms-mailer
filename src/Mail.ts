2b7394210fcc7987000460161f54b62e6ed945ce
Vinicyus <vinicyus346@gmail.com>
2022-02-07 18:14:12 -0300

    feat: sending emails with nodemailer and kafka connection

--- src/Mail.ts
+++ src/Mail.ts
@@ -1,5 +1,14 @@
 import Mail from 'nodemailer';
 
+interface IRequestData {
+  subject: any;  
+  type: any;
+  email: any;
+  name: any;
+  value: any;
+  url?: any;
+}
+
 export default class Mailer {
 
   public transporter;
@@ -22,13 +31,21 @@ export default class Mailer {
     });
   }
 
-  async sendMail(email: string) {
+  async sendMail({subject, type, url, email, name, value}: IRequestData) {    
+
+    const kindMessages: any = {
+      newBet: `<h1> Hello, ${name} </h1> <p> Your bet has been succesfully created, amount: R$ ${value} ! </p>`,      
+      newAccount: `<h1> Hello, ${name} </h1> <p> Welcome to our site, feel free to make your bets when and wherever you want! </>`,
+      noBet: `<h1> Hello, ${name} </h1 <p> It's been some time since your last bet with us, come to our new Site do checkout our new categories! </> `,
+      forgotPassword: `<h1> Forgot Password </h1 <h2> Hello, ${name} </h2> <p> Your request to change your password has been received, click <a href = ${url}> 
+      here </a> bellow to change the password. If it wasn't you, consider change your password. </p> `
+    }
+
     const info = await this.transporter.sendMail({
       from: 'Bet-Lotery <admin@bet.lotery.com>',
       to: email,
-      subject: "New Bet",
-      text: "Thank You for the new bet",
-      html: `<h1> Hello, ${email} </h1> <p> Your bet has been succesfully created, amount: R$ ${ 30 } ! </p>`,
+      subject: subject,      
+      html: kindMessages[type],
       headers: { 'x-myheader': 'New Bet Header' }
     });
 
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,42 @@
 import 'dotenv/config';
 import express, { Request, Response } from 'express';
+import { Kafka } from 'kafkajs';
 import Mailer from './Mail';
 
 const app = express();
 
-app.post('/send/:email', async (request: Request, response: Response) => {
-  const { email } = request.params;
-
-  const transporter = new Mailer();
-
-  const info = await transporter.sendMail(email);
-
-  return response.json(info);  
+const kafka = new Kafka({
+  clientId: 'ms-emails',
+  brokers: ['kafka:29092']
 });
 
+const topic = 'newbet';
+const consumer = kafka.consumer({ groupId: 'newbet-admins' });
 
-app.listen(3333, () => {
-  console.log("Server started at port 3333!");
-});
+const mailer = new Mailer();
+
+async function sendNewBetEmailsToAdmin() {
+  console.log("Send new bet has listen")
+  await consumer.connect();
+  await consumer.subscribe({ topic, fromBeginning: true }).then(() => {
+    console.log("\n \n subscribed! \n \n")
+  });
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message}) => {            
+      const data = message.value!.toString();
+      const dataJson = JSON.parse(data)      
+
+      await mailer.sendMail({
+        subject: dataJson['subject'],
+        type: dataJson['type'],        
+        email: dataJson['email'],
+        name: dataJson['username'],
+        value: dataJson['value']
+      });      
+    }
+  });
+}
+
+sendNewBetEmailsToAdmin();
+
+app.listen(3000);
